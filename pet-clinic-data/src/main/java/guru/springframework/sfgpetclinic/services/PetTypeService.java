beb3358ce43798330b4e110c0fd76b2130d3d05d
reynolds.blair <reynoldsblair@gmail.com>
2019-04-30 16:44:11 -0400

    added PetService, closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by ed.blair on 4/30/2019.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}

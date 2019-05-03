c6f33c66f4cb8e0421729e635baec1fe7be107a4
reynolds.blair <reynoldsblair@gmail.com>
2019-05-02 21:48:52 -0400

    Refactored owner as JPA entity, closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by ed.blair on 4/18/2019.
  */
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by ed.blair on 4/18/2019.
  */
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
